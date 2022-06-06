bd5fbbc5d0443c111343b875bc968be0c87385df
BhatNishanthGanesh <102415498+BhatNishanthGanesh@users.noreply.github.com>
2022-06-06 22:39:45 +0530

    Update fix_me.c

--- fix_me.c
+++ fix_me.c
@@ -6,10 +6,10 @@ int main() {
     int number1, number2, sum, multi;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    scanf("%d%d", &number1, &number2);
 
     // calculating sum
-    sum = number1 - number2;      
+    sum = number1 + number2;      
 
     // Multiply number 1 and number 2
     multi = number1 * number1
